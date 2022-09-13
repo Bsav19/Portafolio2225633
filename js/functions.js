8c5c886043a96e49f8fd37f2831790e9211f7f21
Bsav19 <113374708+Bsav19@users.noreply.github.com>
2022-09-12 22:19:55 -0500

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,31 @@
+function classToggle() {
+    var el = document.querySelector('.icon-cards__content');
+    el.classList.toggle('step-animation');
+  }
+  
+//   document.querySelector('#toggle-animation').addEventListener('click', classToggle);
+  
+
+
+  (function() {
+  
+    var slideContainer = $('.slide-container');
+    
+    slideContainer.slick();
+    
+    $('.clash-card__image img').hide();
+    $('.slick-active').find('.clash-card img').fadeIn(200);
+    
+    // On before slide change
+    slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+      $('.slick-active').find('.clash-card img').fadeOut(1000);
+    });
+    
+    // On after slide change
+    slideContainer.on('afterChange', function(event, slick, currentSlide) {
+      $('.slick-active').find('.clash-card img').fadeIn(200);
+    });
+    
+  })();
+  
+  
